509a836cbe7083a79181dbc662040c141f737089
shakyaarpit <arpitshakya9956@gmail.com>
2025-06-06 07:19:49 +0530

    add static file

--- index.js
+++ index.js
@@ -188,3 +188,8 @@ app.delete("/bookingDelete/:id", async (req, res) => {
   let bookingDel = await Booking.findByIdAndDelete({ _id: id });
   res.json("delete");
 });
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
